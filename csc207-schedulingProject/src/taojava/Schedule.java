41f86c88975307e759fbd4d408f7713025979e8d
yazan <kittaneh17@grinnell.edu>
2014-11-19 17:38:54 -0600

    Added the pseudo code from the wiki page

--- csc207-schedulingProject/src/taojava/Schedule.java
+++ csc207-schedulingProject/src/taojava/Schedule.java
@@ -9,4 +9,40 @@ public interface Schedule
   
   public void output();
    
+  
+  
+  /*
+   * 
+   * //Pseudo Code for Algorithm
+   * 
+   * 
+      History cal = new Calander();
+      int setPriority =1;
+      
+      for(int dateIndex; dateIndex < dates.length(); dateIndex++)
+        {
+          if(matchCounter <= 5)
+            {
+             for(int schoolIndex=0; schoolIndex < schools.length(); schoolIndex++)
+                {
+                  if(hasPlayed(schools[schoolIndex])!= 0 &&
+                     schools[schoolIndex].dates[dateIndex].priority = setPriority) //  if the school's priority is at 'must'
+                    {
+                      for(int i=schoolIndex; i < schools.length(); i++)
+                         {
+                           if(hasPlayed(schools[schoolIndex])!= 0 &&
+                             schools[i].dates[dateIndex].priority = setPriority)
+                             {
+                               setMatch(schools[schoolIndex], schools[i], dates[dateIndex]); //sets a match, taking in two schools and a date
+                             } // if the second school has not played, and the date priority = setPriority
+                         }// for: a second available opponent
+                    }// if the school has not played, and the date is at priority = setPriority
+                  setPriority = 2;
+                }//For: a first available school
+            }//if: number of matches scheduled for each date is less than or equal to 5
+        }
+   * 
+   * 
+   * 
+   */
 }// itnerface Schedule
